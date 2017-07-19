fd1194f071c9a315d332087a38c89ce2bfcf7e29
anandc <anand.c80@gmail.com>
2017-07-19 18:51:23 +0400

    Dockerize the app

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,6 +1,6 @@
 import initialState from './initialState';
 import * as types from '../constants/actionTypes';
-export default function (state = initialState.products, action) {
+export default (state = initialState.products, action) => {
   switch (action.type) {
   	case types.RECEIVE_PRODUCTS:
   		const { payload } = action;
